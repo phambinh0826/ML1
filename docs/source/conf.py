d1086e2db0acbdb78b272231d46622721674b58f
BinhPham0826 <ptcb1012588646@gmail.com>
2025-03-09 22:04:22 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,35 +1,52 @@
 # Configuration file for the Sphinx documentation builder.
+import sphinx_rtd_theme
 
 # -- Project information
-
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Machine Learning 1'
+copyright = '2025, Bình Phạm'
+author = 'Bình Phạm'
 
 release = '0.1'
 version = '0.1.0'
 
 # -- General configuration
-
 extensions = [
+    'nbsphinx',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx_rtd_theme',
+    'sphinx_copybutton',
+    'sphinx_gallery.load_style',
 ]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
-intersphinx_disabled_domains = ['std']
 
+# Add any paths that contain templates here, relative to this directory.
+# The template links to a custom style css to increase the width of displayed content
 templates_path = ['_templates']
 
 # -- Options for HTML output
-
+# -- The theme to use for HTML and HTML Help pages.  See the documentation for a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
+
+# -- Path to static files
+html_static_path = ['_static']
+
+master_doc = 'index'
+
+highlight_language = 'python3'
+
+nbsphinx_execute_arguments = [
+    "--InlineBackend.figure_formats={'svg', 'pdf'}",
+    "--InlineBackend.rc={'figure.dpi': 96}",
+]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
